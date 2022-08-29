3d873cd1df965b719b0b5b26d593e882200ba465
Fast <carlos.augusto.fast@hp.com>
2022-08-29 15:43:47 -0300

    smeegs

--- jsAlgos.js
+++ jsAlgos.js
@@ -4,6 +4,10 @@ const sumResultSelector = document.getElementById('sum-result');
 const sumBtn = document.getElementById('sum-btn');
 
 function sum () {
+  console.log('---SUM CALLED?---')
+  // '1'
+  // +'1'
+  // 1
   sumResultSelector.value = +sumNum1Selector.value + +sumNum2Selector.value;
 }
 
